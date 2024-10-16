4e4585ffb66e4389c120993d49b499cc1af324ff
Marmik patel <62463824+marmik28@users.noreply.github.com>
2024-10-16 18:20:00 -0400

    Added user entity

---
+++ src/main/java/com/marmik/backend/config/JacksonConfig.java
@@ -0,0 +1,21 @@
+package com.marmik.backend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.marmik.backend.serializers.ObjectIdSerializer;
+import org.bson.types.ObjectId;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(ObjectId.class, new ObjectIdSerializer());
+        objectMapper.registerModule(module);
+        return objectMapper;
+    }
+}
